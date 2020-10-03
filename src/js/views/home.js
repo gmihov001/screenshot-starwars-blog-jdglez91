a6183f28ff67e7a2785ed898d4fadacaafd138d6
George Mihov <gmihov001@gmail.com>
2020-10-03 20:50:33 +0000

    Updates

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -13,24 +13,6 @@ export class Home extends React.Component {
 		this.state = {};
 	}
 
-	componentDidMount() {
-		// fetch("https://swapi.dev/api/planets/")
-		// 	.then(function(response) {
-		// 		if (!response.ok) {
-		// 			throw Error(response.statusText);
-		// 		}
-		// 		// Read the response as json.
-		// 		return response.json();
-		// 	})
-		// 	.then(function(responseAsJson) {
-		// 		// Do stuff with the JSON
-		// 		console.log("responseAsJson", responseAsJson);
-		// 		this.setState({ planets: responseAsJson.results });
-		// 	})
-		// 	.catch(function(error) {
-		// 		console.log("Looks like there was a problem: \n", error);
-		// 	});
-	}
 	render() {
 		return (
 			<div>
